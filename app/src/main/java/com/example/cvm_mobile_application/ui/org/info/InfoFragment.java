27770a1be9e6e7aa30f79440d3153be908db87c3
dangnghiepcuong <dangnghiepcuong@gmail.com>
2023-03-28 21:27:46 +0700

    rename resources

---
+++ app/src/main/java/com/example/cvm_mobile_application/ui/org/info/InfoFragment.java
@@ -0,0 +1,2 @@
+package com.example.cvm_mobile_application.ui.org.info;public class InfoFragment {
+}
---
+++ app/src/main/java/com/example/cvm_mobile_application/ui/org/schedule/ScheduleFragment.java
@@ -0,0 +1,2 @@
+package com.example.cvm_mobile_application.ui.org.schedule;public class ScheduleFragment {
+}
