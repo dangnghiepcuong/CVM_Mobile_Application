33dcd301fa2be03342669e9b10c457344b9ebec1
dangnghiepcuong <dangnghiepcuong@gmail.com>
2023-04-18 09:42:16 +0700

    fix ui bug

--- app/src/main/java/com/example/cvm_mobile_application/MainActivity.java
+++ app/src/main/java/com/example/cvm_mobile_application/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.cvm_mobile_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.example.cvm_mobile_application.ui.citizen.home.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        HomeFragment citizenHome = new HomeFragment();
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction =manager.beginTransaction();
+        transaction.replace(R.id.fragment_citizen_home, citizenHome);
+        transaction.commit();
     }
 }
