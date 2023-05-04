85a6b64e30881d93e05f145a5580e0e80a3795b7
Đặng Nghiệp Cường <91720005+dangnghiepcuong@users.noreply.github.com>
2023-05-04 23:01:06 +0700

    login funtion (#44)
    Co-authored-by: Chrysanths <112877815+Chrysanths@users.noreply.github.com>

--- app/src/main/java/com/example/cvm_mobile_application/MainActivity.java
+++ app/src/main/java/com/example/cvm_mobile_application/MainActivity.java
@@ -1,34 +1,94 @@
 package com.example.cvm_mobile_application;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.cvm_mobile_application.ui.citizen.HomePage;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private Button btn_login;
+    private EditText et_username;
+    private EditText et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_login = findViewById(R.id.btn_login);
+        et_username = findViewById(R.id.et_username);
+        et_password = findViewById(R.id.et_password);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
-        /*HomeFragment citizenHome = new HomeFragment();
 
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction =manager.beginTransaction();
-        transaction.replace(R.id.fragment_citizen_home, citizenHome);
-        transaction.commit();*/
-        Button btn_login = findViewById(R.id.btn_login);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            startActivity(new Intent(getApplicationContext(), HomePage.class));
+            finish();
+            return;
+        }
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), HomePage.class));
-                finish();
+                String username = String.valueOf(et_username.getText());
+
+                if(username.equals("")){
+                    Toast.makeText(MainActivity.this, "*Nhập tài khoản", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String password = String.valueOf(et_password.getText());
+                if(password.equals("")){
+                    Toast.makeText(MainActivity.this, "*Nhập mật khẩu", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(username, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    Toast.makeText(MainActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    startActivity(new Intent(getApplicationContext(), HomePage.class));
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_LONG).show();
+                                    return;
+                                }
+                            }
+                        });
+
+
             }
         });
     }
