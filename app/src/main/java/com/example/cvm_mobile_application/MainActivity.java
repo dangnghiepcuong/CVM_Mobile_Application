d28c4712e09e0e7c5e71ee8efc17a631b683f3ec
Chrysanths <112877815+Chrysanths@users.noreply.github.com>
2023-05-04 22:54:15 +0700

    login funtion

--- app/src/main/java/com/example/cvm_mobile_application/MainActivity.java
+++ app/src/main/java/com/example/cvm_mobile_application/MainActivity.java
@@ -13,12 +13,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.cvm_mobile_application.ui.citizen.HomePage;
+import com.example.cvm_mobile_application.ui.citizen.CitizenHome;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,8 +48,8 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            startActivity(new Intent(getApplicationContext(), HomePage.class));
+        if (currentUser != null) {
+            startActivity(new Intent(getApplicationContext(), CitizenHome.class));
             finish();
             return;
         }
@@ -56,41 +59,63 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String username = String.valueOf(et_username.getText());
 
-                if(username.equals("")){
+                if (username.equals("")) {
                     Toast.makeText(MainActivity.this, "*Nhập tài khoản", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 String password = String.valueOf(et_password.getText());
-                if(password.equals("")){
+                if (password.equals("")) {
                     Toast.makeText(MainActivity.this, "*Nhập mật khẩu", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                mAuth.createUserWithEmailAndPassword(username, password)
+                mAuth.signInWithEmailAndPassword(username, password)
                         .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "createUserWithEmail:success");
+                                    Log.d(TAG, "signInWithEmail:success");
                                     Toast.makeText(MainActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
                                     FirebaseUser user = mAuth.getCurrentUser();
-                                    startActivity(new Intent(getApplicationContext(), HomePage.class));
-                                    finish();
+
                                 } else {
                                     // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
                                     Toast.makeText(MainActivity.this, "Authentication failed.",
                                             Toast.LENGTH_LONG).show();
                                     return;
                                 }
                             }
                         });
-
-
             }
         });
     }
 
+    public void updateUI(String username) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Integer role = -1;
+        db.collection("acounts")
+                .whereEqualTo("username", username)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    public Integer role;
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                role = (Integer) document.get("role");
+
+
+                            }
+                        } else {
+                        }
+                    }
+                }).getResult();
+        startActivity(new Intent(getApplicationContext(), CitizenHome.class));
+        Intent intent = new Intent(MainActivity.this, CitizenHome.class);
+        //intent.putExtra();
+        finish();
+    }
 }
