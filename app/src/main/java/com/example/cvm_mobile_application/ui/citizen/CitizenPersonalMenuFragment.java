29d00a08e8f558e80c495004562d7b520d4ab0c4
dangnghiepcuong <dangnghiepcuong@gmail.com>
2023-05-06 06:10:28 +0700

    implement new citizen screens: notification screen, personal menu screen

--- app/src/main/java/com/example/cvm_mobile_application/ui/citizen/CitizenPersonalMenuFragment.java
+++ app/src/main/java/com/example/cvm_mobile_application/ui/citizen/CitizenPersonalMenuFragment.java
@@ -11,16 +11,19 @@ import androidx.fragment.app.Fragment;
 import com.example.cvm_mobile_application.R;
 import com.example.cvm_mobile_application.data.db.model.Citizen;
 
-public class CitizenHomeFragment extends Fragment {
+public class CitizenPersonalMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_citizen_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_citizen_personal_menu, container, false);
 
         Citizen citizen = getArguments().getParcelable("citizen");
 
         TextView fullName = view.findViewById(R.id.FullName);
         fullName.setText(citizen.getFull_name());
+
+        TextView phoneNumber = view.findViewById(R.id.PhoneNumber);
+        phoneNumber.setText(citizen.getPhone());
         return view;
     }
 }
