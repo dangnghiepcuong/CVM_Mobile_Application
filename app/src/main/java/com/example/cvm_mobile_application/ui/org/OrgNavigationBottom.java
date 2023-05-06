b3038769aab64252d6bf0dbe80b78381e2ba8d19
Chrysanths <112877815+Chrysanths@users.noreply.github.com>
2023-05-06 09:55:33 +0700

    rename

--- app/src/main/java/com/example/cvm_mobile_application/ui/org/OrgNavigationBottom.java
+++ app/src/main/java/com/example/cvm_mobile_application/ui/org/OrgNavigationBottom.java
@@ -9,35 +9,34 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.cvm_mobile_application.R;
-import com.example.cvm_mobile_application.ui.org.home.HomeFragment;
-import com.example.cvm_mobile_application.ui.org.home.InstructionFragment;
-import com.example.cvm_mobile_application.ui.org.home.NotificationFragment;
-import com.example.cvm_mobile_application.ui.org.info.InfoFragment;
-import com.example.cvm_mobile_application.ui.org.schedule.ScheduleFragment;
+import com.example.cvm_mobile_application.ui.citizen.notification.NotificationFragment;
+import com.example.cvm_mobile_application.ui.org.home.OrgHomeFragment;
+import com.example.cvm_mobile_application.ui.org.info.OrgOptionalMenuFragment;
+import com.example.cvm_mobile_application.ui.org.schedule.OrgScheduleFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class OrgHome extends AppCompatActivity {
+public class OrgNavigationBottom extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.navigation_bottom_org);
-        replaceFragment(new com.example.cvm_mobile_application.ui.org.home.HomeFragment());
+        setContentView(R.layout.org_navigation_bottom);
+        replaceFragment(new OrgHomeFragment());
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation_org);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
                 case R.id.home:
-                    replaceFragment(new HomeFragment());
+                    replaceFragment(new OrgHomeFragment());
                     break;
                 case R.id.info:
-                    replaceFragment(new InfoFragment());
+                    replaceFragment(new OrgOptionalMenuFragment());
                     break;
                 case R.id.notification:
                     replaceFragment(new NotificationFragment());
                     break;
                 case R.id.schedule:
-                    replaceFragment(new ScheduleFragment());
+                    replaceFragment(new OrgScheduleFragment());
                     break;
             }
             return true;
--- app/src/main/java/com/example/cvm_mobile_application/ui/org/info/OrgOptionalMenuFragment.java
+++ app/src/main/java/com/example/cvm_mobile_application/ui/org/info/OrgOptionalMenuFragment.java
@@ -1,22 +1,19 @@
-package com.example.cvm_mobile_application.ui.org.home;
+package com.example.cvm_mobile_application.ui.org.info;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.cvm_mobile_application.R;
 
-public class HomeFragment extends Fragment {
+public class OrgOptionalMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_org_home, container, false);
+        return inflater.inflate(R.layout.fragment_org_optional_menu, container, false);
     }
 }
