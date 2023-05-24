05f0b41c9899a1e712a3bc0cad72cb9053775716
dangnghiepcuong <dangnghiepcuong@gmail.com>
2023-05-24 23:26:36 +0700

    change directories

--- app/src/main/java/com/example/cvm_mobile_application/ui/citizen/CitizenPersonalMenuFragment.java
+++ app/src/main/java/com/example/cvm_mobile_application/ui/citizen/CitizenPersonalMenuFragment.java
@@ -9,43 +9,67 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.example.cvm_mobile_application.R;
 import com.example.cvm_mobile_application.data.db.model.Citizen;
-import com.example.cvm_mobile_application.ui.admin.info.ProfileFragment;
-import com.example.cvm_mobile_application.ui.citizen.info.CitizenProfileFragment;
-
-import org.checkerframework.checker.units.qual.Current;
+import com.example.cvm_mobile_application.ui.citizen.info.CitizenProfileActivity;
 
 public class CitizenPersonalMenuFragment extends Fragment {
+
+    private LinearLayout menuTabProfile;
+    private Citizen citizen;
+    private View view;
+    private LinearLayout menuTabLogout;
+    private TextView fullName;
+    private TextView phoneNumber;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_citizen_personal_menu, container, false);
+        view = inflater.inflate(R.layout.fragment_citizen_personal_menu, container, false);
 
-        Citizen citizen = getArguments().getParcelable("citizen");
+        citizen = getArguments().getParcelable("citizen");
 
-        TextView fullName = view.findViewById(R.id.FullName);
+
+
+        return view;
+    }
+
+    public void implementView() {
+        fullName = view.findViewById(R.id.FullName);
         fullName.setText(citizen.getFull_name());
 
-        TextView phoneNumber = view.findViewById(R.id.PhoneNumber);
+        phoneNumber = view.findViewById(R.id.PhoneNumber);
         phoneNumber.setText(citizen.getPhone());
 
-        LinearLayout profile = view.findViewById(R.id.profile);
-        profile.setOnClickListener(new View.OnClickListener() {
+        menuTabProfile = view.findViewById(R.id.menu_tab_profile);
+
+        menuTabLogout = view.findViewById(R.id.menu_tab_logout);
+    }
+
+    public void setViewListener() {
+        menuTabProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getProfileActivity(citizen);
             }
         });
-        return view;
+
+        menuTabLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     public void getProfileActivity(Citizen citizen){
-        Intent intent = new Intent(getActivity(), CitizenProfileFragment.class);
+        Intent intent = new Intent(getActivity(), CitizenProfileActivity.class);
         intent.putExtra("citizen", citizen);
         startActivity(intent);
+    }
+
+    public void logOut() {
+
     }
 }
