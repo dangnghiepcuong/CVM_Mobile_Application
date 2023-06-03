bd2cbbafec7767da314195db9fb80537daf45508
Chrysanths <112877815+Chrysanths@users.noreply.github.com>
2023-06-03 20:19:42 +0700

    org profile 70%

--- app/src/main/java/com/example/cvm_mobile_application/ui/org/info/OrgProfileActivity.java
+++ app/src/main/java/com/example/cvm_mobile_application/ui/org/info/OrgProfileActivity.java
@@ -1,16 +1,12 @@
-package com.example.cvm_mobile_application.ui.citizen.info;
+package com.example.cvm_mobile_application.ui.org.info;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -19,10 +15,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cvm_mobile_application.R;
 import com.example.cvm_mobile_application.data.SpinnerOption;
-import com.example.cvm_mobile_application.data.db.model.Citizen;
+import com.example.cvm_mobile_application.data.db.model.Organization;
 import com.example.cvm_mobile_application.data.objects.DVHCHelper;
 import com.example.cvm_mobile_application.ui.SpinnerAdapter;
 import com.example.cvm_mobile_application.ui.ViewStructure;
+import com.example.cvm_mobile_application.ui.citizen.info.CitizenProfileActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -34,14 +31,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class CitizenProfileActivity extends AppCompatActivity implements ViewStructure {
-    private EditText etFullName;
-    private RadioButton rdBtnGenderMale;
-    private RadioButton rdBtnGenderFemale;
-    private RadioButton rdBtnGenderAnother;
-    private EditText etPhone;
+public class OrgProfileActivity extends AppCompatActivity implements ViewStructure {
+    private FirebaseFirestore db;
+    private DVHCHelper dvhcHelper;
+    private Organization org;
+
     private EditText etId;
-    private EditText etEmail;
+    private EditText etName;
     private Spinner spProvince;
     private List<SpinnerOption> provinceList = new ArrayList<>();
     private SpinnerAdapter spProvinceListAdapter;
@@ -51,27 +47,20 @@ public class CitizenProfileActivity extends AppCompatActivity implements ViewStr
     private Spinner spWard;
     private List<SpinnerOption> wardList = new ArrayList<>();
     private SpinnerAdapter spWardListAdapter;
-    private DVHCHelper dvhcHelper;
     private EditText etStreet;
-    private TextView tvBirthday;
-    private Button btnBirthdayDP;
-    private DatePicker dpBirthday;
-    private Citizen citizen;
     private Button btnSaveProfile;
-    private RadioGroup rdGroupGender;
-    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_citizen_profile);
+        setContentView(R.layout.fragment_org_profile);
         db = FirebaseFirestore.getInstance();
         dvhcHelper = new DVHCHelper(getApplicationContext());
     }
 
     protected void onStart() {
         super.onStart();
-        citizen = getIntent().getParcelableExtra("citizen");
+        org = getIntent().getParcelableExtra("org");
 
         try {
             implementView();
@@ -80,23 +69,13 @@ public class CitizenProfileActivity extends AppCompatActivity implements ViewStr
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
-    }
 
+    }
     @Override
     public void implementView() {
-        etFullName = findViewById(R.id.et_fullname);
-
-        tvBirthday = findViewById(R.id.tv_birthday);
-        btnBirthdayDP = findViewById(R.id.btn_birthday_dp);
-        dpBirthday = findViewById(R.id.dp_birthday);
-
-        rdGroupGender = findViewById(R.id.rd_group_gender);
-        rdBtnGenderMale = findViewById(R.id.rd_btn_gender_male);
-        rdBtnGenderFemale = findViewById(R.id.rd_btn_gender_female);
-        rdBtnGenderAnother = findViewById(R.id.rd_btn_gender_another);
-        etPhone = findViewById(R.id.et_phone);
         etId = findViewById(R.id.et_id);
-        etEmail = findViewById(R.id.et_email);
+        etName = findViewById(R.id.et_name);
+
         spProvince = findViewById(R.id.sp_province);
         spDistrict = findViewById(R.id.sp_district);
         spWard = findViewById(R.id.sp_ward);
@@ -107,23 +86,10 @@ public class CitizenProfileActivity extends AppCompatActivity implements ViewStr
 
     @Override
     public void bindViewData() throws JSONException {
-        etFullName.setText(citizen.getFull_name());
-        tvBirthday.setText(citizen.getBirthday());
+        etId.setText(org.getId());
+        etId.setEnabled(false);
 
-        switch (citizen.getGender()) {
-            case "Nam":
-                rdBtnGenderMale.setChecked(true);
-                break;
-            case "Nữ":
-                rdBtnGenderFemale.setChecked(true);
-                break;
-            case "Khác":
-                rdBtnGenderAnother.setChecked(true);
-                break;
-        }
-        etPhone.setText(citizen.getPhone());
-        etId.setText(citizen.getId());
-        etEmail.setText(citizen.getEmail());
+        etName.setText(org.getName());
 
         //GET PROVINCE LIST
         provinceList = dvhcHelper.getLocalList(DVHCHelper.PROVINCE_LEVEL, null);
@@ -133,7 +99,7 @@ public class CitizenProfileActivity extends AppCompatActivity implements ViewStr
                 R.layout.item_string, provinceList);
         spProvince.setAdapter(spProvinceListAdapter);
         int provincePosition = dvhcHelper.getLocalPositionFromList(
-                DVHCHelper.PROVINCE_LEVEL, citizen.getProvince_name(), null);
+                DVHCHelper.PROVINCE_LEVEL, org.getProvince_name(), null);
         spProvince.setSelection(provincePosition);
 
         //GET DISTRICT LIST
@@ -145,7 +111,7 @@ public class CitizenProfileActivity extends AppCompatActivity implements ViewStr
                 R.layout.item_string, districtList);
         spDistrict.setAdapter(spDistrictListAdapter);
         int districtPosition = dvhcHelper.getLocalPositionFromList(
-                DVHCHelper.DISTRICT_LEVEL, citizen.getDistrict_name(), provinceCode);
+                DVHCHelper.DISTRICT_LEVEL, org.getDistrict_name(), provinceCode);
         spDistrict.setSelection(districtPosition);
 
         //GET WARD LIST
@@ -157,35 +123,14 @@ public class CitizenProfileActivity extends AppCompatActivity implements ViewStr
                 R.layout.item_string, wardList);
         spWard.setAdapter(spWardListAdapter);
         int wardPosition = dvhcHelper.getLocalPositionFromList(
-                DVHCHelper.WARD_LEVEL, citizen.getWard_name(), districtCode);
+                DVHCHelper.WARD_LEVEL, org.getWard_name(), districtCode);
         spWard.setSelection(wardPosition);
 
-        etStreet.setText(citizen.getStreet());
+        etStreet.setText(org.getStreet());
     }
 
     @Override
     public void setViewListener() {
-
-        btnBirthdayDP.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (dpBirthday.getVisibility() == View.GONE) {
-                    dpBirthday.setVisibility(View.VISIBLE);
-                } else {
-                    dpBirthday.setVisibility(View.GONE);
-                }
-            }
-        });
-
-        dpBirthday.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
-            @Override
-            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                monthOfYear++;
-                tvBirthday.setText(year + "-" + monthOfYear + "-" + dayOfMonth);
-            }
-        });
-
-        //SET PROVINCE SPINNER LISTENER
         spProvince.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -267,31 +212,15 @@ public class CitizenProfileActivity extends AppCompatActivity implements ViewStr
         btnSaveProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                CitizenProfileActivity.this.updateProfile();
+                OrgProfileActivity.this.updateProfile();
             }
         });
     }
 
-    public void updateProfile() {
-        Citizen profile = new Citizen();
-        profile.setFull_name(String.valueOf(etFullName.getText()));
-        profile.setBirthday(String.valueOf(tvBirthday.getText()));
-
-        switch (rdGroupGender.getCheckedRadioButtonId()) {
-            case 0:
-                profile.setGender("Nam");
-                break;
-            case 1:
-                profile.setGender("Nữ");
-                break;
-            default:
-                profile.setGender("Khác");
-                break;
-        }
-
-        profile.setPhone(String.valueOf(etPhone.getText()));
-        profile.setId(String.valueOf(etId.getText()));
-        profile.setEmail(String.valueOf(etEmail.getText()));
+    public void updateProfile(){
+        Organization profile = new Organization();
+//        profile.setId(String.valueOf(etId.getText()));
+        profile.setName(String.valueOf(etName.getText()));
 
         SpinnerOption spOption = (SpinnerOption) spProvince.getSelectedItem();
         profile.setProvince_name(spOption.getOption());
@@ -305,30 +234,27 @@ public class CitizenProfileActivity extends AppCompatActivity implements ViewStr
         profile.setStreet(String.valueOf(etStreet.getText()));
 
         Map<String, Object> data = new HashMap<>();
-        data.put("full_name", profile.getFull_name());
-        data.put("birthday", profile.getBirthday());
-        data.put("gender", profile.getGender());
-        data.put("phone", profile.getPhone());
-        data.put("id", profile.getId());
+        data.put("name", profile.getName());
         data.put("province_name", profile.getProvince_name());
         data.put("district_name", profile.getDistrict_name());
         data.put("ward_name", profile.getWard_name());
         data.put("street", profile.getStreet());
 
-        db.collection("users")
-                .document(citizen.getPhone())
+        db.collection("organizations")
+                .document(org.getId())
                 .set(data)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(CitizenProfileActivity.this,
+                            Toast.makeText(OrgProfileActivity.this,
                                     "Cập nhật thông tin thành công!", Toast.LENGTH_SHORT).show();
                         } else {
-                            Toast.makeText(CitizenProfileActivity.this,
+                            Toast.makeText(OrgProfileActivity.this,
                                     "Đã có lỗi xảy ra", Toast.LENGTH_LONG).show();
                         }
                     }
                 });
+
     }
 }
