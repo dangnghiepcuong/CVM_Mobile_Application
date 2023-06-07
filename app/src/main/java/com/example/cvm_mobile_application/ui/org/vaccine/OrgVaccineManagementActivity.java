363d742b274a5bb7a3ed3632e507286a9feb2c43
Chrysanths <112877815+Chrysanths@users.noreply.github.com>
2023-06-08 01:44:33 +0700

    change xml include layout add toolbar2 for vaccine managerment

--- app/src/main/java/com/example/cvm_mobile_application/ui/org/vaccine/OrgVaccineManagementActivity.java
+++ app/src/main/java/com/example/cvm_mobile_application/ui/org/vaccine/OrgVaccineManagementActivity.java
@@ -2,7 +2,9 @@ package com.example.cvm_mobile_application.ui.org.vaccine;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -11,15 +13,17 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.cvm_mobile_application.R;
 import com.example.cvm_mobile_application.data.db.model.Organization;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class OrgVaccineManagementActivity extends AppCompatActivity {
 
-    private LinearLayout btnVaccineInventory;
-    private LinearLayout btnVaccineImport;
     private OrgVaccineInventoryFragment orgVaccineInventoryFragment;
     private OrgVaccineImportFragment orgVaccineImportFragment;
     private Organization org;
     private Bundle bundle;
+    private BottomNavigationView toolbarMenu;
+    private Button btnBack;
+    private TextView tbTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,7 @@ public class OrgVaccineManagementActivity extends AppCompatActivity {
         bundle.putParcelable("org", org);
 
         implementView();
+        bindViewData();
         setViewListener();
 
         orgVaccineInventoryFragment = new OrgVaccineInventoryFragment();
@@ -43,27 +48,39 @@ public class OrgVaccineManagementActivity extends AppCompatActivity {
     }
 
     public void implementView() {
-        btnVaccineInventory = findViewById(R.id.menu_tab_vaccine_inventory);
-        btnVaccineImport = findViewById(R.id.menu_tab_vaccine_import);
+        toolbarMenu = findViewById(R.id.toolbar_menu);
+        btnBack = findViewById(R.id.btn_back);
+        tbTitle = findViewById(R.id.tb_title);
+    }
+
+    public void bindViewData(){
+        tbTitle.setText("Quản lý kho vắc-xin");
+        toolbarMenu.getMenu().findItem(R.id.menu_option1).setTitle("Kho vắc-xin");
+        toolbarMenu.getMenu().findItem(R.id.menu_option2).setTitle("Nhập vắc-xin");
     }
 
     public void setViewListener() {
-        btnVaccineInventory.setOnClickListener(new View.OnClickListener() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                orgVaccineInventoryFragment = new OrgVaccineInventoryFragment();
-                orgVaccineInventoryFragment.setArguments(bundle);
-                replaceFragment(orgVaccineInventoryFragment);
+            public void onClick(View view) {
+                finish();
             }
         });
 
-        btnVaccineImport.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                orgVaccineImportFragment = new OrgVaccineImportFragment();
-                orgVaccineImportFragment.setArguments(bundle);
-                replaceFragment(orgVaccineImportFragment);
+        toolbarMenu.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.menu_option1:
+                    orgVaccineInventoryFragment = new OrgVaccineInventoryFragment();
+                    orgVaccineInventoryFragment.setArguments(bundle);
+                    replaceFragment(orgVaccineInventoryFragment);
+                    break;
+                case R.id.menu_option2:
+                    orgVaccineImportFragment = new OrgVaccineImportFragment();
+                    orgVaccineImportFragment.setArguments(bundle);
+                    replaceFragment(orgVaccineImportFragment);
+                    break;
             }
+            return true;
         });
     }
 
