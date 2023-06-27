10036e81979acc760a4e478cd09fa44f3e99e270
dangnghiepcuong <dangnghiepcuong@gmail.com>
2023-06-28 03:36:00 +0700

    View, Features: Citizen Vaccination Profile

--- app/src/main/java/com/example/cvm_mobile_application/ui/org/schedule/registration_management/OrgViewVaccinationProfile.java
+++ app/src/main/java/com/example/cvm_mobile_application/ui/org/schedule/registration_management/OrgViewVaccinationProfile.java
@@ -1,16 +1,19 @@
 package com.example.cvm_mobile_application.ui.org.schedule.registration_management;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.cvm_mobile_application.R;
 import com.example.cvm_mobile_application.data.db.model.Citizen;
 import com.example.cvm_mobile_application.ui.ViewStructure;
+import com.example.cvm_mobile_application.ui.citizen.registration.CitizenRegistrationFragment;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class OrgViewVaccinationProfile extends AppCompatActivity implements ViewStructure {
@@ -23,7 +26,8 @@ public class OrgViewVaccinationProfile extends AppCompatActivity implements View
     private TextView tvGender;
     private TextView tvPhone;
     private TextView tvId;
-    private TextView tvAdress;
+    private TextView tvAdrress;
+    private CitizenRegistrationFragment citizenRegistrationFragment;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +56,8 @@ public class OrgViewVaccinationProfile extends AppCompatActivity implements View
         tvGender = findViewById(R.id.tv_gender);
         tvPhone = findViewById(R.id.tv_phone);
         tvId = findViewById(R.id.tv_id);
-        tvAdress = findViewById(R.id.tv_address);
+        tvAdrress = findViewById(R.id.tv_address);
+
     }
 
     @Override
@@ -68,16 +73,25 @@ public class OrgViewVaccinationProfile extends AppCompatActivity implements View
         String adress = citizen.getStreet() + ", "
                 + citizen.getWard_name() + ", "
                 + citizen.getProvince_name();
-        tvAdress.setText(adress);
+        tvAdrress.setText(adress);
+
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("citizen", citizen);
+        bundle.putString("fromActivity", "org");
+        citizenRegistrationFragment = new CitizenRegistrationFragment();
+        citizenRegistrationFragment.setArguments(bundle);
+        replaceFragment(citizenRegistrationFragment);
     }
 
     @Override
     public void setViewListener() {
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        btnBack.setOnClickListener(view -> finish());
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
